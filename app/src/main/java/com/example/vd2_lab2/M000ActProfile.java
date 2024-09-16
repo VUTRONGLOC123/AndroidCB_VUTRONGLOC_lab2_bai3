a8f0d2cafea40d120903890aaac4726560943a0b
vutrongloc <vutrongloc641112@gmail.com>
2024-09-16 19:46:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/vd2_lab2/M000ActProfile.java
@@ -0,0 +1,14 @@
+package com.example.vd2_lab2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class M000ActProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m001_act_profile); // Tham chiếu đến layout m001_act_profile.xml
+    }
+}
